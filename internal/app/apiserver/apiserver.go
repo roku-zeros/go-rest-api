66c590cb097f6742169ec1e9376f888c68ae1d8c
Zerogi <rob.zgonnikov@gmail.com>
2023-01-16 17:17:25 +0300

    - add configs - add logger

--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,11 +1,35 @@
 package apiserver
 
+import "github.com/sirupsen/logrus"
+
 type ApiServer struct {
+	config *Config
+	logger *logrus.Logger
 }
 
-func New() *ApiServer {
-	return &ApiServer{}
+func New(config *Config) *ApiServer {
+	return &ApiServer{
+		config: config,
+		logger: logrus.New(),
+	}
 }
 func (s *ApiServer) Start() error {
+	if err := s.configureLogger(); err != nil {
+		return err
+	}
+
+	s.logger.Info("starting api server")
+
+	return nil
+}
+
+func (s *ApiServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
+
 	return nil
 }
